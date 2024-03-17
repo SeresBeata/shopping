460a91ffdd0ef20f6644887c5c7f7d3a0f127c87
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-17 16:08:58 +0100

    Read Data from the Firestore Database
    Update ShoppingLists.js file. Import useEffect, collection, getDocs. Create an async function fetchShoppingLists. Call fetchShoppingLists() async function inside useEffect().

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,10 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
+// import the getDocs and collections functions
+import { collection, getDocs } from 'firebase/firestore';
 
 //create and export ShoppingLists child component
 const ShoppingLists = ({ db }) => {
   //create state variable with initial state empty array
   const [lists, setLists] = useState([]);
+
+  //getting all documents of a collection by  creating an async function outside useEffect()
+  const fetchShoppingLists = async () => {
+    const listsDocuments = await getDocs(collection(db, 'shoppinglists'));
+    let newLists = [];
+    listsDocuments.forEach((docObject) => {
+      newLists.push({ id: docObject.id, ...docObject.data() });
+    });
+    setLists(newLists);
+  };
+
+  //use useEffect
+  // call fetchShoppingLists() async function inside useEffect()
+  useEffect(() => {
+    fetchShoppingLists();
+  }, [`${lists}`]);
 
   return (
     <View style={styles.container}>
