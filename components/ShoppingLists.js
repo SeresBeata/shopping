9bf2fa37fbc9334f59ee487b90947b094d7535d9
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-17 16:34:04 +0100

    Create style at ShoppingLists.js
    Create style for elements at ShoppingLists.js file.

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -46,6 +46,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  listItem: {
+    height: 70,
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: '#AAA',
+    flex: 1,
+    flexGrow: 1,
+  },
+  listForm: {
+    flexBasis: 275,
+    flex: 0,
+    margin: 15,
+    padding: 15,
+    backgroundColor: '#CCC',
+  },
+  listName: {
+    height: 50,
+    padding: 15,
+    fontWeight: '600',
+    marginRight: 50,
+    marginBottom: 15,
+    borderColor: '#555',
+    borderWidth: 2,
+  },
+  item: {
+    height: 50,
+    padding: 15,
+    marginLeft: 50,
+    marginBottom: 15,
+    borderColor: '#555',
+    borderWidth: 2,
+  },
+  addButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: '#000',
+    color: '#FFF',
+  },
+  addButtonText: {
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 20,
+  },
 });
 
 export default ShoppingLists;
