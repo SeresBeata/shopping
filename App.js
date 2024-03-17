1e1d63a6e69c5886420bd9af152591e4f8cf1ed1
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-17 14:21:33 +0100

    Initialize connection to Firestore
    Import initializeApp() and getFirestore(). Add Firebase configuration. Initialize Firebase and Cloud Firestore.

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
+//To initialize a connection for Firestore import initializeApp() and getFirestore()
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const App = () => {
+  //Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCSHiJcNpDgX6Me6n0rFV-LgK4TwbmKo0U',
+    authDomain: 'shopping-list-demo-37a23.firebaseapp.com',
+    projectId: 'shopping-list-demo-37a23',
+    storageBucket: 'shopping-list-demo-37a23.appspot.com',
+    messagingSenderId: '756067040239',
+    appId: '1:756067040239:web:84ef7846ee7ee92146541a',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
