5f37049061a8bd87ed80c70f0a11f016b289b106
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-17 15:27:30 +0100

    Update ShoppingLists for list
    Import useState and FlatList. Extract the "db" prop in the ShoppingLists component's function definition. Create state variable "lists" with initial state empty array. Use FlatList component to display list. Use lists state at FlatList. Use array function join() to convert array into a string. Create style for View.

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,8 +1,33 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { View, FlatList } from 'react-native';
 
 //create and export ShoppingLists child component
-const ShoppingLists = () => {
-  return <View></View>;
+const ShoppingLists = ({ db }) => {
+  //create state variable with initial state empty array
+  const [lists, setLists] = useState([]);
+
+  return (
+    <View style={styles.container}>
+      {/* use FlatList component to display list  */}
+      <FlatList
+        //use lists state
+        data={lists}
+        renderItem={({ item }) => (
+          <Text>
+            {/* use array function join() that converts an array into a string and adds a separator string between each element from the array once concatenated */}
+            {item.name}: {item.items.join(', ')}
+          </Text>
+        )}
+      />
+    </View>
+  );
 };
+
+//create style
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default ShoppingLists;
